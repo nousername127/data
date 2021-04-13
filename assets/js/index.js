613fbfc274490188c36b10abd0bc2a6c977d1d45
nousername127 <3261404635@qq.com>
2021-04-13 15:03:31 +0800

    fullpage

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+			$('#fullpage').fullpage({
+				//options here
+				autoScrolling:true,
+				scrollHorizontally: true,
+				keyboardScrolling:true,
+				paddingTop:0,
+				navigation:true,
+				loopBottom:true,
+				loopTop:true
+			});
+		
+			//methods
+			//$.fn.fullpage.setAllowScrolling(false);
+		});
